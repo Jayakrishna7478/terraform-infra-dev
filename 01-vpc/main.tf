5214f002aa0a2c7ffc8265292c4edb8c4099de9d
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2024-02-08 20:13:11 +0530

    roboshop-infra-prod

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -6,6 +6,7 @@ module "roboshop" {
   environment = var.environment
   common_tags = var.common_tags
   vpc_tags = var.vpc_tags
+  vpc_cidr = var.vpc_cidr
   public_subnets_cidr = var.public_subnets_cidr
   private_subnets_cidr = var.private_subnets_cidr
   database_subnets_cidr = var.database_subnets_cidr
