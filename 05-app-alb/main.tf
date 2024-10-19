433b639b63f901d8d3472757c868635269ec0256
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2024-10-19 07:57:54 +0530

    jenkins

--- 05-app-alb/main.tf
+++ 05-app-alb/main.tf
@@ -29,20 +29,20 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-# route 53 records
-module "records" {
-  source  = "terraform-aws-modules/route53/aws//modules/records"
+# # route 53 records
+# module "records" {
+#   source  = "terraform-aws-modules/route53/aws//modules/records"
 
-  zone_name = var.zone_name
+#   zone_name = var.zone_name
 
-  records = [
-    {
-      name    = "*.app-${var.environment}"
-      type    = "A"
-      alias   = {
-        name    = aws_lb.app_alb.dns_name
-        zone_id = aws_lb.app_alb.zone_id
-      }
-    }
-  ]
-}
+#   records = [
+#     {
+#       name    = "*.app-${var.environment}"
+#       type    = "A"
+#       alias   = {
+#         name    = aws_lb.app_alb.dns_name
+#         zone_id = aws_lb.app_alb.zone_id
+#       }
+#     }
+#   ]
+# }
